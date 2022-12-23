4917904eea6d5aded735dafe9d7581cc30e882b3
Pranav Shastri <sahstripranav@gmail.com>
2022-12-23 13:36:34 +0530

    OG

--- src/main/java/com/wileyedge/java_oop_2_7/Main.java
+++ src/main/java/com/wileyedge/java_oop_2_7/Main.java
@@ -1,6 +1,6 @@
 package com.wileyedge.java_oop_2_7;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hell, World!");
+        System.out.println("HellOO, World!");
     }
 }
